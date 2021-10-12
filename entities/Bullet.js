f5007e8b54ba5a1f8318c35c16525cc14c51c881
vanArthur <vanackere.arthur@gmail.com>
2021-10-12 18:41:41 +0200

    moved entities to own folder, dynamic playersize

---
+++ entities/Bullet.js
@@ -0,0 +1,44 @@
+import { cubeCollision } from "../helperFunctions/collision.js";
+
+export class Bullet {
+  constructor(pos, id, shooterId, vel, playerSize) {
+    this.pos = pos;
+    this.shooter = shooterId;
+    this.id = id;
+    this.vel = vel;
+    this.shot = [];
+    this.newBullet = true;
+  }
+
+  move() {
+    this.pos.add(this.vel);
+    this.newBullet = false;
+  }
+  deltaMove(delta) {
+    this.pos.set(
+      this.pos.x + this.vel.x * delta,
+      this.pos.y + this.vel.y * delta
+    );
+  }
+  checkOutOfBounds() {
+    return (
+      this.pos.x > 1500 || this.pos.x < 0 || this.pos.y < 0 || this.pos.y > 1000
+    );
+  }
+
+  checkPlayerCollision(players) {
+    for (var playerId in players) {
+      if (!this.shot.includes(playerId)) {
+        const player = players[playerId];
+        if (cubeCollision(player.pos, playerSize, playerSize, this.pos, 5, 5)) {
+          if (playerId !== this.shooter) {
+            player.reduceHealth(1);
+            this.shot.push(playerId);
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+}
