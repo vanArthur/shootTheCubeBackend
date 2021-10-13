69d893238837c1e8914f38dc346c1131b1088eea
vanArthur <vanackere.arthur@gmail.com>
2021-10-13 20:17:56 +0200

    better randomId generator

--- helperFunctions/randomId.js
+++ helperFunctions/randomId.js
@@ -1,16 +1,3 @@
-import { randomInt } from "crypto";
-
-let randomIds = new Set();
-
 export function randomId() {
-  let string = "";
-  for (let i = 0; i < 10; i++) {
-    string = string + randomInt(9).toString();
-  }
-  let setLenght = randomIds.size;
-  randomIds.add(string);
-  if (setLenght === randomIds.size) {
-    string = randomId();
-  }
-  return string;
+  return Math.random().toString(36).slice(2);
 }
